2cbecac1c9ddb68601920a0061e020f4a74372aa
ChaneaSimmons <167230329+ChaneaSimmons@users.noreply.github.com>
2024-06-05 14:50:42 -0600

    updated api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("Poem generated");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -10,18 +9,15 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
+
   let instructionsInput = document.querySelector("#user-instructions");
+
   let apiKey = "aatbf0c2od53d41737ca6d7d30e64b01";
   let context =
-    "You are a romantic Poem expert and you love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions.";
-  let prompt = ` User instructions: Generate a English poem about ${instructionsInput.value}`;
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a English poem about ${instructionsInput}`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log("apiUrl");
-  console.log("Generating Poem");
-  console.log(`Prompt:${prompt}`);
-  console.log(`Context:${context}`);
-
   axios.get(apiUrl).then(displayPoem);
 }
 
